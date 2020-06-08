71872c7625b4bce12ad6041d3019118ca383a375
Antonio Reyes <reytono@gmail.com>
2020-06-08 11:43:56 -0600

    componentes

--- angular-crud-app/src/app/app.module.ts
+++ angular-crud-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPersonComponent } from './search-person/search-person.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { InsertPersonComponent } from './insert-person/insert-person.component';
+import { HomeComponent } from './home/home.component';
+import { DeletePersonComponent } from './delete-person/delete-person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPersonComponent,
+    NavBarComponent,
+    InsertPersonComponent,
+    HomeComponent,
+    DeletePersonComponent
   ],
   imports: [
     BrowserModule,
