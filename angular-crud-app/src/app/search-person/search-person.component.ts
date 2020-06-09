cf614da314b3213e7e7f6ea49e2c5b56cfe5cd21
Antonio Reyes <reytono@gmail.com>
2020-06-09 16:15:20 -0600

    Update search-person-component

--- angular-crud-app/src/app/search-person/search-person.component.ts
+++ angular-crud-app/src/app/search-person/search-person.component.ts
@@ -30,21 +30,74 @@ export class SearchPersonComponent implements OnInit {
 
   getPersonsById(idValue: string){
     this.personsList = [];
-    this.http.get(this.ROOT_URL + '/' + idValue).toPromise().then(data => {
-      console.log(data);
-      /*
+    this.http.get(this.ROOT_URL).toPromise().then(data => {
       const count = Object.keys(data).length;
-      for (let n = 0; n < count; n++){
+
+      for (let n = 0; n < count; n++) {
         const sendToList = {
-          idPersona: idValue, nombrePersona: data[n].nombre, apellidoPersona: data[n].apellido, fechaNacimiento: data[n].nacimiento
+          idPersona: data[n].id, nombrePersona: data[n].nombre, apellidoPersona: data[n].apellido, fechaNacimiento: data[n].nacimiento
         };
-        this.personsList.push(sendToList);
+        const lookingForId = sendToList.idPersona;
+        if (lookingForId === idValue){
+          this.personsList.push(sendToList);
+        }
       }
-       */
+    });
+  }
+
+  getPersonByName(nameValue: string){
+    this.personsList = [];
+    this.http.get(this.ROOT_URL).toPromise().then(data => {
+      const count = Object.keys(data).length;
+
+      for (let n = 0; n < count; n++) {
+        const sendToList = {
+          idPersona: data[n].id, nombrePersona: data[n].nombre, apellidoPersona: data[n].apellido, fechaNacimiento: data[n].nacimiento
+        };
+        const lookingForId = sendToList.nombrePersona;
+        if (lookingForId === nameValue){
+          this.personsList.push(sendToList);
+        }
+      }
+    });
+  }
+
+  getPersonByLastName(lastnameValue: string){
+    this.personsList = [];
+    this.http.get(this.ROOT_URL).toPromise().then(data => {
+      const count = Object.keys(data).length;
+
+      for (let n = 0; n < count; n++) {
+        const sendToList = {
+          idPersona: data[n].id, nombrePersona: data[n].nombre, apellidoPersona: data[n].apellido, fechaNacimiento: data[n].nacimiento
+        };
+        const lookingForId = sendToList.apellidoPersona;
+        if (lookingForId === lastnameValue){
+          this.personsList.push(sendToList);
+        }
+      }
+    });
+  }
+
+  getPersonByDate(dateValue: string){
+    this.personsList = [];
+    this.http.get(this.ROOT_URL).toPromise().then(data => {
+      const count = Object.keys(data).length;
+
+      for (let n = 0; n < count; n++) {
+        const sendToList = {
+          idPersona: data[n].id, nombrePersona: data[n].nombre, apellidoPersona: data[n].apellido, fechaNacimiento: data[n].nacimiento
+        };
+        const lookingForId = sendToList.fechaNacimiento;
+        if (lookingForId === dateValue){
+          this.personsList.push(sendToList);
+        }
+      }
     });
   }
 
   ngOnInit(): void {
+    this.getPersons();
   }
 
 }
